96ab4e9ec3598e3cadc5d280a4435f8936b2be5c
amdaaraijen <bintangtriadmaja@gmail.com>
2025-10-20 20:42:35 +0700

    fix: enable no explicit any on useRef

--- src/components/ParallaxLayout.tsx
+++ src/components/ParallaxLayout.tsx
@@ -8,10 +8,10 @@ import generateStars from "@/utils/generateStats";
 import Stars from "./Stars";
 import ContactSection from "./ContactSection";
 import { useRef } from "react";
-import type { Parallax as ParallaxType } from "@react-spring/parallax";
 
 export default function ParallaxLayout() {
-  const parallaxRef = useRef<typeof ParallaxType | null>(null);
+  //eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const parallaxRef = useRef<any>(null);
 
   const stars = generateStars(25);
 
